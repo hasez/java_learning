e255def455fd0e34f3beb89e62334873f7826be9
hasez <hasehaseo@gmail.com>
2022-06-13 15:28:51 +0000

    Add lesson6 trainning

---
+++ app.codility.com/src/main/java/sort/Sort.java
@@ -0,0 +1,42 @@
+package sort;
+
+public class Sort {
+    public int[] selectionSort(int[] A) {
+        for (int k = 0; k < A.length; k++) {
+            int minimal = k;
+            for (int j = k + 1; j < A.length; j++) {
+                if (A[j] < A[minimal]) {
+                    minimal = j;
+                }
+            }
+            // swap A[k], A[minimal]
+            int tmp = A[minimal];
+            A[minimal] = A[k];
+            A[k] = tmp;
+        }
+        return A;
+    }
+    public int[] countingSort(int[] A) {
+        int min = 0;
+        int max = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < min) {
+                min = A[i];
+            }
+            if (max < A[i]) {
+                max = A[i];
+            }
+        }
+        int[] count = new int[max - min + 1];
+        for (int i = 0; i<A.length; i++) {
+            count[ A[i] - min ]++;
+        }
+        int p = 0;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                A[p++] = i + min;
+            }
+        }
+        return A;
+    }
+}
---
+++ app.codility.com/src/test/java/sort/SortTest.java
@@ -0,0 +1,25 @@
+package sort;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SortTest {
+    private int[] A = {5, 2, 8, 14, 1, -1, 0, 0, -15, 16};
+    private int[] expected = {-15, -1, 0, 0, 1, 2, 5, 8, 14, 16};
+
+    @Before
+    public void beforeEach() {
+    }
+
+    @Test
+    public void testSelectionSort() {
+        int[] actual = new Sort().selectionSort(A);
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testCountingSort() {
+        int[] actual = new Sort().countingSort(A);
+        assertArrayEquals(expected, actual);
+    }
+}
